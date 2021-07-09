aa3a07ebb3cb89b1e9c0cc1fdba1dc77bbf23211
jade <e2goon@gmail.com>
2021-07-09 19:02:46 +0900

    feat: 스키마 변경 감지 기능
    - 수정 시 타입과 쿼리가 재생성

--- src/hooks/usePosts/index.ts
+++ src/hooks/usePosts/index.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 import { useQuery } from "react-query";
-import { RepositoryQuery as RepositoryModel } from "../../generated/types";
-import { RepositoryQuery } from "../../generated/queries";
+import { RepositoryQuery } from "@/generated/types";
+import { repository } from "@/generated/queries";
 
 // TODO: 리팩토링하기
 const client = new GraphQLClient(process.env.NEXT_PUBLIC_GITHUB_ENDPOINT, {
@@ -11,7 +11,7 @@ const client = new GraphQLClient(process.env.NEXT_PUBLIC_GITHUB_ENDPOINT, {
 });
 
 const fetchPosts = async () => {
-  const data: RepositoryModel = await client.request(RepositoryQuery);
+  const data: RepositoryQuery = await client.request(repository);
   const posts = data.repository.issues.nodes;
   return posts;
 };
