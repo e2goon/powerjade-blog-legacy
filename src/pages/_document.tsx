eb1749a10fc67307f1f3158396a33c722c8cd382
jade <e2goon@gmail.com>
2021-07-20 00:44:53 +0900

    refactor: lang="ko" 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
   }
   render() {
     return (
-      <Html>
+      <Html lang="ko">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
