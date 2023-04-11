74af1b4e7dffc35fee991808a97102c15fb1c793
wonmin.lee <e2goon@gmail.com>
2023-04-11 22:50:18 +0900

    fix: 개발, 프로덕션 환경에 따라 포스트 노출 구분
    - 개발모드일 땐 parallel 하게 스크립트 실행
    - 빌드모드일 땐 series 하게 스크립트 실행

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import Link from "next/link";
 import { github } from "~/utils/client";
 
 export default async function Home() {
-  const issues = await github.issues({ owner: "e2goon", name: "powerjade" });
+  const isDev = process.env.NODE_ENV === "development";
+  const issues = await github.issues({
+    owner: "e2goon",
+    name: "powerjade",
+    label: isDev ? ["draft", "published"] : ["published"],
+  });
   const posts = issues.repository?.issues.nodes;
 
   if (!posts) {
